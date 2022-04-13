3102bfef78fa4be63c74335f444a9eb209807e1d
Calvin Turner <turnerjrcalvin@gmail.com>
2022-04-13 13:29:47 -0400

    httplink

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -6,7 +6,22 @@ import Navbar from './components/Navbar';
 import {ApolloClient, InMemoryCache,ApolloProvider, createHttpLink,} from "@apollo/client";
 import {setContext} from "@apollo/client/link/context";
 
-const 
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_,{headers}) => {
+  const token =localstorage.getItem("id_token");
+  return{
+    headers:{
+      ...headers,
+      authorization: token ? `Bearer ${token}`: "",
+
+    },
+  };
+});
+
+const clie
 
 
 
